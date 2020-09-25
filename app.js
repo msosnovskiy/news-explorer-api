5f142794f8c6b732e9e04314223bc256adbe6361
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-09-25 10:18:43 +0300

    added user Schema

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/news-explorer', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Указан неверный адрес почтового ящика',
+    },
+    required: [true, 'Emaik является обязательным для заполения'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Пароль является обязательным для заполения'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, 'Имя является обязательным полем для заполения'],
+    minlength: [2, 'минимальное количество символов - 2'],
+    maxlength: [30, 'максимальное количество символов - 30'],
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
