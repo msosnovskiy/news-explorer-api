f0ad9db3a8e9b080fc7bd3b01d93a52b9d59ce8c
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-09-27 12:01:26 +0300

    added logger

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const helmet = require('helmet');
+// const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 const users = require('./routes/users');
 const articles = require('./routes/articles');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(helmet());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useUnifiedTopology: true,
@@ -20,6 +25,8 @@ mongoose.connect('mongodb://localhost:27017/news-explorer', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
@@ -29,6 +36,8 @@ app.use('/articles', auth, articles);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
+
+app.use(errorLogger);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
