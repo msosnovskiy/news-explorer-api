0b7a5f4575953235042142817791a1c3a320925f
Maksim Sosnovskiy <SosnovskiyMV@quarta.su>
2020-09-29 10:02:09 +0300

    edit ratelimit message and rules

--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,14 +2,14 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: ({ message: 'С этого IP-адреса отправлено слишком много запросов, повторите попытку позже' }),
+  max: 200,
+  message: ({ message: 'Отправлено слишком много запросов, повторите попытку позже' }),
 });
 
 const createAccountLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 10,
-  message: ({ message: 'Отправлено слишком много запросов на создание учетной записи. Повторите попытку через час' }),
+  windowMs: 5 * 60 * 1000,
+  max: 5,
+  message: ({ message: 'Отправлено слишком много запросов, повторите попытку позже' }),
 });
 
 module.exports = { apiLimiter, createAccountLimiter };
